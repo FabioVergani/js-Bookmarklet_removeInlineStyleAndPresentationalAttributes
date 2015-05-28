868dca6abef00151ee66639a2ebded6c09ba9177
FabioVergani <fabio.vergani.1975@gmail.com>
2015-05-28 15:54:50 +0200

    Update removeInlineStyleAndPresentationalAttributes.js

--- removeInlineStyleAndPresentationalAttributes.js
+++ removeInlineStyleAndPresentationalAttributes.js
@@ -16,6 +16,10 @@ javascript:(function(d){
 })(document);
 //
 
+
+//removeInlineStyleAndPresentationalAttributes
+javascript:(function(d){var e,k,m=d.getElementsByTagName('*'),i=m.length+0,p=['align','background','bgcolor','border','cellpadding','cellspacing','color','face','height','hspace','marginheight','marginwidth','noshade','nowrap','valign','vspace','width','vlink','alink','text','link','frame','frameborder','clear','scrolling','style'],j=p.length+0;while(i--!==0){e=m[i];k=j+0;while(k--!==0){e.removeAttribute(p[k]);};};})(document);
+
 /*
 <a href="javascript:
 (function(d){
